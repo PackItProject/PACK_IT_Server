c3b33ea077285ac27b4695f5179bba28bb0f726d
lumiere-on <mjw0214@ewhain.net>
2024-02-06 11:50:59 +0900

    Update order API

--- src/controllers/map.controller.js
+++ src/controllers/map.controller.js
@@ -19,6 +19,8 @@ exports.postBookmarkInList=(req,res, next)=>{
     res.json(response);
 }
 exports.getStoreIdInMap=(req,res,next)=>{
+    const orderType = req.query.orderType || 'map';
+
     const storeId=req.body.storeId;
 
     const store=storeData.findByStoreId(storeId);
@@ -33,3 +35,37 @@ exports.getStoreIdInMap=(req,res,next)=>{
     }
 
 }
+exports.postBookmarkInStore=(req,res, next)=>{
+    //사용자가 별 누르면 북마크 됨.
+    const orderType = req.query.orderType || 'map';
+    const { storeId, bookmark } = req.body;
+
+    const response = {
+        storeId: storeId,
+        bookmark: bookmark
+    };
+
+    res.json(response);
+}
+exports.getMenuById=(req,res,next)=>{
+    const orderType = req.query.orderType || 'map';
+    const {storeId, menuId} =req.body.params;
+
+    const menu=storeData.findByMenuId(storeId, menuId);
+
+    const response={
+        storeId: storeId,
+        menuId:menuId,
+        menuName: menu.menuName,
+        menuDescription: menu.menuDescription,
+        price: menu.price,
+        size: menu.size,
+        insulation: menu.insulation,
+        liquid_sealed: menu.liquid_sealed,
+        caution: menu.caution
+    };
+
+    res.json(response);
+}
+
+
--- src/models/storeData.dto.js
+++ src/models/storeData.dto.js
@@ -1,21 +1,3 @@
-module.exports=class storesData {
-    constructor(id, name, latitude, longitude, address, open, rate, bookmark){
-        this.id=id;
-        this.name=name;
-        this.latitude=latitude;
-        this.longitude=longitude;
-        this.address=address;
-        this.open=opne;
-        this.rate=rate;
-        this.bookmark=bookmark;
-    }
-    //storeId로 가게 찾기
-    static findByStoreId(storeId, cb){
-
-    }
-}
-
-//DB에서 stores 불러오기.
 async function getStoresFromDB(){
     try{
         //DB 가져오는 부분.
@@ -29,14 +11,77 @@ async function getStoresFromDB(){
     }
 }
 
-async function findByStoreId(id){
-    try{
-        const stores=await getStoresFromDB();
-
-        const store=stores.find(id => store.id=id);
-
-        return store;
-    }catch(error){
-        console.error('Error finding storeId',error);
+module.exports=class storesData {
+    constructor(id, name, latitude, longitude, address, open, rate, bookmark){
+        this.id=id;
+        this.name=name;
+        this.latitude=latitude;
+        this.longitude=longitude;
+        this.address=address;
+        this.open=opne;
+        this.rate=rate;
+        this.bookmark=bookmark;
+        //1.menuId로 메뉴객체 데려오기
+        //2.meal과 식사류.
     }
+    //storeId로 가게 찾기
+    async findByStoreId(id){
+        try{
+            const stores= await getStoresFromDB();
+
+            const store=stores.find(store => store.id===id);
+
+            return store;
+        }catch(error){
+            console.error('Error finding storeId',error);
+        }
+    }
+
 }
+
+module.exports=class menu{
+    constructor(storeId, menuId, menuName, menuDescription, price, size, insulation, liquid_sealed, caution) {
+        this.storeId=storeId;
+        this.menuId=menuId;
+        this.menuName=menuName;
+        this.menuDescription=menuDescription;
+        this.price=price;
+        this.size=size;
+        this.insulation=insulation;
+        this.liquid_sealed=liquid_sealed;
+        this.caution=caution;
+    }
+
+    async findByStoreId(id){
+        try{
+            const stores= await getStoresFromDB();
+
+            const store=stores.find(store => store.id===id);
+
+            return store;
+        }catch(error){
+            console.error('Error finding storeId',error);
+        }
+    }
+
+    async findByMenuId(storeId, menuId){
+        try{
+            const store=this.findByStoreId(storeId);
+            const menu=store.find(menu=>menu.id===menuId);
+
+            return menu;
+        }
+        catch(error){
+            console.log('Error finding menuId',error);
+        }
+    }
+
+}
+
+module.exports=class meal{
+
+}
+module.exports=class side{
+
+}
+
