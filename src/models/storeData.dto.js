f98c1a267c0e69dddecea5e93e9de4b0d5267e64
lumiere-on <mjw0214@ewhain.net>
2024-02-10 14:27:48 +0900

    Update order API

--- src/models/storeData.dto.js
+++ src/models/storeData.dto.js
@@ -21,8 +21,17 @@ module.exports=class storesData {
         this.open=opne;
         this.rate=rate;
         this.bookmark=bookmark;
+        this.menu=new Map();
+
+
+
         //1.menuId로 메뉴객체 데려오기
         //2.meal과 식사류.
+    }
+
+    addMenu(type, menuId, menuName, description, price, size, insulation, liquidSealed, caution) {
+        const menu = new Menu(menuId, menuName, description, price, size, insulation, liquidSealed, caution);
+        this.menus.set(menuId, menuName);
     }
     //storeId로 가게 찾기
     async findByStoreId(id){
@@ -36,126 +45,45 @@ module.exports=class storesData {
             console.error('Error finding storeId',error);
         }
     }
-}
-
-module.exports=class menu{
-    constructor(storeId, menuId, menuName, menuDescription, price, size, insulation, liquid_sealed, caution) {
-        this.storeId=storeId;
-        this.menuId=menuId;
-        this.menuName=menuName;
-        this.menuDescription=menuDescription;
-        this.price=price;
-        this.size=size;
-        this.insulation=insulation;
-        this.liquid_sealed=liquid_sealed;
-        this.caution=caution;
-    }
-
-    async findByStoreId(id){
-        try{
-            const stores= await getStoresFromDB();
-
-            const store=stores.find(store => store.id===id);
-
-            return store;
-        }catch(error){
-            console.error('Error finding storeId',error);
-        }
-    }
-
     async findByMenuId(storeId, menuId) {
         try{
             const store=this.findByStoreId(storeId);
-            const menu=store.find(menu=>menu.id===menuId);
-
-            return menu;
+            for(const [menuId, menuName] of this.menu.entries()){
+                if(menuId===menuId){
+                    return menuName;
+                }
+            }
         }
         catch(error){
             console.log('Error finding menuId',error);
         }
     }
-
-}
-
-//메뉴 조회.
-module.exports=class meal{
-
-    constructor(storeId,type,menuId, menuName){
-        this.storeId=storeId;
-        this.type=type; //meal or side
-        this.menuId=menuId;
-        this.menuName=menuName;
-    }
-    async findByStoreId(id){
-        try{
-            const stores= await getStoresFromDB();
-
-            const store=stores.find(store => store.id===id);
-
-            return store;
-        }catch(error){
-            console.error('Error finding storeId',error);
-        }
-    }
-    async showMeal(storeId){
+    async showMeal(storeId) {
         try {
-            const store = await findByStoreId(storeId);
-            const meal = store.findAll(); //이러면 메뉴 다 불러올 수 있는지 확인.
-
-            return meal;
-        }
-        catch(error){
-            console.error('Error showing meal',error);
+            const store = await this.findByStoreId(storeId); // this를 사용하여 메서드를 호출합니다.
+            const meal=[];
+            for (const [key, value] of this.menus.entries()) {
+                if (value.type === 'meal') {
+                    return value; // 'meal'인 메뉴를 배열에 추가합니다.
+                }
+            }
+        } catch (error) {
+            console.error('Error showing meal', error);
+            throw error; // 오류를 다시 throw하여 호출자에게 전파합니다.
         }
     }
-
-
+    async showSide(storeId) {
+        try {
+            const store = await this.findByStoreId(storeId); // this를 사용하여 메서드를 호출합니다.
+            const side=[];
+            for (const [key, value] of this.menus.entries()) {
+                if (value.type === 'side') {
+                    return value; // 'meal'인 메뉴를 배열에 추가합니다.
+                }
+            }
+        } catch (error) {
+            console.error('Error showing meal', error);
+            throw error; // 오류를 다시 throw하여 호출자에게 전파합니다.
+        }
+    }
 }
-//메뉴조회.
-module.exports=class side{
-
-    constructor(storeId,type,menuId, menuName){
-        this.storeId=storeId;
-        this.type=type; //meal or side
-        this.menuId=menuId;
-        this.menuName=menuName;
-    }
-
-    async findByStoreId(id){
-        try{
-            const stores= await getStoresFromDB();
-
-            const store=stores.find(store => store.id===id);
-
-            return store;
-        }catch(error){
-            console.error('Error finding storeId',error);
-        }
-    }
-
-    async findByStoreId(id){
-        try{
-            const stores= await getStoresFromDB();
-
-            const store=stores.find(store => store.id===id);
-
-            return store;
-        }catch(error){
-            console.error('Error finding storeId',error);
-        }
-    }
-    async showSide(storeId){
-
-        try{
-        const store=await findByStoreId(storeId);
-        const side=store.findAll();
-
-        return side;
-        }
-        catch(error){
-            console.error('Error showing meal',error);
-        }
-    }
-
-}
-
