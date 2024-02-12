15d41eeaec1f013f73981d0831ddc13cc78df993
gayeon7877 <gayean01@gmail.com>
2024-02-12 21:34:06 +0900

    ✨ [Feat] 가게 북마크 설정 API 구현

--- src/controllers/bookmark.controller.js
+++ src/controllers/bookmark.controller.js
@@ -1,4 +1,4 @@
-import {getBookmarkedStoreService} from "../services/bookmark.service.js";
+import {getBookmarkedStoreService, postBookmarkService} from "../services/bookmark.service.js";
 
 //북마크된 가게 조회
 export const getBookmarkedStoreController = async (req, res, next) => {
@@ -10,4 +10,17 @@ export const getBookmarkedStoreController = async (req, res, next) => {
         next(error);
     }
 };
+
+//북마크 설정
+export const postBookmarkController = async (req, res, next) => {
+    const { store_id, pk_user } = req.params;
+    try {
+        await postBookmarkService(store_id, pk_user);
+        res.status(200).send({ message: '북마크가 성공적으로 변경되었습니다.' });
+    } catch (error) {
+        next(error);
+    }
+};
+
+
 
--- src/services/bookmark.service.js
+++ src/services/bookmark.service.js
@@ -2,14 +2,51 @@ import {pool} from "../../config/db.config.js";
 
 export const getBookmarkedStoreService = async (pk_user) => {
     const query = `
-        SELECT store.store_name, store.status, AVG(G.grade) as average_grade, store.address,store.bookmark
+        SELECT store.store_id,store.store_name, store.status, AVG(G.grade) as average_grade, store.address
         FROM store
         INNER JOIN PackIt.Grade G on store.store_id = G.store_id
-        INNER JOIN PackIt.Bookmark on store.store_id = Bookmark.store_id
-        WHERE store.bookmark=1 and Bookmark.pk_user=?
-        GROUP BY store.store_name, store.status, store.address
+        INNER JOIN PackIt.Bookmark B on store.store_id = B.store_id
+        WHERE B.pk_user=?
+        GROUP BY store.store_id, store.store_name, store.status, store.address
+
     `;
 
     const [result] = await pool.execute(query,[pk_user]);
     return result;
+};
+
+//북마크 설정
+export const postBookmarkService = async (store_id, pk_user) => {
+    const connection = await pool.getConnection();
+
+    try {
+        await connection.beginTransaction();
+
+        const checkQuery = `
+            SELECT * FROM Bookmark
+            WHERE store_id = ? AND pk_user = ?
+        `;
+        const [checkResult] = await connection.execute(checkQuery, [store_id, pk_user]);
+
+        if (checkResult.length > 0) {
+            const deleteQuery = `
+                DELETE FROM Bookmark
+                WHERE store_id = ? AND pk_user = ?
+            `;
+            await connection.execute(deleteQuery, [store_id, pk_user]);
+        } else {
+            const insertQuery = `
+                INSERT INTO Bookmark (store_id, pk_user)
+                VALUES (?, ?)
+            `;
+            await connection.execute(insertQuery, [store_id, pk_user]);
+        }
+
+        await connection.commit();
+    } catch (error) {
+        await connection.rollback();
+        throw error;
+    } finally {
+        connection.release();
+    }
 };
