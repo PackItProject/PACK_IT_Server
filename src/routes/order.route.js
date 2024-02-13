f83b6528efb3351f1b3f5d2b16a43ea0b77a2e4a
lumiere-on <mjw0214@ewhain.net>
2024-02-13 18:01:16 +0900

    Update bookmark and store API

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -13,15 +13,14 @@ orderRouter.get('?orderType=map/near/:store_id',getStoreInfoController);
 orderRouter.post('r?orderType=map/near/:store_id/bookmark',postBookmarkController);
 
 orderRouter.get('?orderType=map/near/:store_id/:menu_id}',getMenuController);
-orderRouter.get('?orderType=map/near/:store-id/meal',showMealController);
-orderRouter.get('?orderType=map/near/:store-id/side',showSideController);
-orderRouter.get('?orderType=map/search/:store-id');
+orderRouter.get('?orderType=map/near/:store_id/meal',showMealController);
+orderRouter.get('?orderType=map/near/:store_id/side',showSideController);
+orderRouter.get('?orderType=map/search/:store_id');
 
 
 //북마크에서 가게 접근
-orderRouter.get('?orderType=bookmark/');
-orderRouter.get('?orderType=bookmark/:bookmarkId&:store-id');
-orderRouter.get('?orderType=bookmark/:bookmarkId=1&:store-id/:menu-id');
-orderRouter.get('?orderType=bookmark/:bookmarkId=1&:store-id/meal');
-orderRouter.get('?orderType=bookmark/:bookmarkId=1&:store-id/side');
+orderRouter.get('?orderType=bookmark/:store_id',getStoreInfoController);
+orderRouter.get('?orderType=bookmark/:store_id/:menu-id',getMenuController);
+orderRouter.get('?orderType=bookmark/:store_id/meal',showMealController);
+orderRouter.get('?orderType=bookmark/:store_id/side',showSideController);
 
