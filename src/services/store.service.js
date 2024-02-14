1a285bb150a625318e7a822874afcf99230ece19
0-x-14 <81631374+0-x-14@users.noreply.github.com>
2024-02-14 21:07:21 +0900

    [Feat] 가게 리뷰 조회 API 구현 (#82)

--- src/services/store.service.js
+++ src/services/store.service.js
@@ -7,3 +7,11 @@ export const getStoreInfo = async (store_id) => {
 
     return rows[0];
 }
+
+export const getStoreGrade = async (store_id) => {
+    const query = ' SELECT u.name, g.grade, g.content, g.image FROM User u JOIN Grade g ON u.pk_user = g.pk_user WHERE g.store_id = ?'
+    const params = [store_id];
+    const rows = await pool.execute(query, params);
+
+    return rows[0];
+}
