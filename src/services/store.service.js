92b83e3dd16344624c271d8a2139fc4eb3502749
0-x-14 <81631374+0-x-14@users.noreply.github.com>
2024-02-16 18:31:42 +0900

    [Fix] 가게 리뷰 조회 API 수정 #94 (#96)
    * [Fix] 가게 정보 조회, 가게 리뷰 조회 API 명세서 수정 (#92)
    
    * [Fix] 가게 리뷰 조회 API 수정 (#95)

--- src/services/store.service.js
+++ src/services/store.service.js
@@ -1,7 +1,8 @@
 import { pool } from '../../config/db.config.js';
 
 export const getStoreInfo = async (store_id) => {
-    const query = ' SELECT store.* FROM store WHERE store_id = ?';
+    const query = ' SELECT s.* FROM store s WHERE store_id = ?';
+    // const query = ' SELECT s.store_name, s.tel, s.address, s.boss, s.license, s.hours, s.introduction, s.notice FROM store s WHERE store_id = ?';
     const params = [store_id];
     const rows = await pool.execute(query, params);
 
@@ -9,7 +10,7 @@ export const getStoreInfo = async (store_id) => {
 }
 
 export const getStoreGrade = async (store_id) => {
-    const query = ' SELECT u.name, g.grade, g.content, g.image FROM User u JOIN Grade g ON u.pk_user = g.pk_user WHERE g.store_id = ?'
+    const query = 'SELECT s.store_id, s.store_name, g.nickname, g.grade, g.content, g.image FROM store s JOIN Grade g ON s.store_id = g.store_id WHERE s.store_id = ?';
     const params = [store_id];
     const rows = await pool.execute(query, params);
 
