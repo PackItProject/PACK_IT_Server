9df21b2912d1d029797ae4592a22891864549251
0-x-14 <0x14@daum.net>
2024-02-16 23:34:24 +0900

    [Fix] 가게 정보 조회, 가게 리뷰 조회 API 쿼리 수정

--- src/services/store.service.js
+++ src/services/store.service.js
@@ -1,8 +1,7 @@
 import { pool } from '../../config/db.config.js';
 
 export const getStoreInfo = async (store_id) => {
-    const query = ' SELECT s.* FROM store s WHERE store_id = ?';
-    // const query = ' SELECT s.store_name, s.tel, s.address, s.boss, s.license, s.hours, s.introduction, s.notice FROM store s WHERE store_id = ?';
+    const query = ' SELECT s.store_name, s.tel, s.address, s.boss, s.license, s.hours, s.introduction, s.notice FROM store s WHERE store_id = ?';
     const params = [store_id];
     const rows = await pool.execute(query, params);
 
@@ -10,7 +9,7 @@ export const getStoreInfo = async (store_id) => {
 }
 
 export const getStoreGrade = async (store_id) => {
-    const query = 'SELECT s.store_id, s.store_name, g.nickname, g.grade, g.content, g.image FROM store s JOIN Grade g ON s.store_id = g.store_id WHERE s.store_id = ?';
+    const query = 'SELECT g.nickname, g.grade, g.content, g.image FROM Grade g JOIN store s ON g.store_id = s.store_id WHERE s.store_id = ?';
     const params = [store_id];
     const rows = await pool.execute(query, params);
 
