c10836fbfde8bb3cdba9feec93bbd4e23853d7c7
lumiere-on <mjw0214@ewhain.net>
2024-02-17 14:03:41 +0900

    Update Bookmark API

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -12,9 +12,5 @@ orderRouter.get('/order/map/near/:store_id/side',showSideController);
 orderRouter.get('/order/map/search/:store_name', searchByStoreNameController);
 
 
-//북마크에서 가게 접근
-orderRouter.get('/order/bookmark/:store_id',getByStoreIdController);
-orderRouter.get('/order/bookmark/:store_id/menu/:menu_id',getMenuController);
-orderRouter.get('/order/bookmark/:store_id/meal',showMealController);
-orderRouter.get('/order/bookmark/:store_id/side',showSideController);
+
 
--- src/services/bookmark.service.js
+++ src/services/bookmark.service.js
@@ -50,3 +50,67 @@ export const postBookmarkService = async (store_id, pk_user) => {
         connection.release();
     }
 };
+
+export const getBookmarkedStoreList=async(pk_user)=>{
+    const query=`
+        SELECT store.store_id, store.store_name, store.grade, store.status, store.address, store.image, Bookmark.pk_user
+        From store
+        INNER JOIN Bookmark on store.store_id=Bookmark.store_id
+        WHERE Bookmark.pk_user=? 
+    `;
+
+    const [rows]=await pool.execute(query, [pk_user]);
+
+    return rows;
+};
+export const getBookmarkedByStoreId = async (store_id, pk_user) => {
+    const query=`
+        SELECT store.store_id, store.image, menu.menu_category,menu.id, menu.menu_name, menu.image,menu.price, menu.containter, Bookmark.pk_user
+        FROM store
+                 INNER JOIN menu on store.store_id=menu.store_id
+                 INNER JOIN Bookmark on store.store_id=Bookmark.store_id
+        WHERE store.store_id=? AND Bookmark.pk_user=?
+    `;
+    const params = [store_id, pk_user];
+    const [rows] = await pool.execute(query, params);
+
+    return rows;
+}
+
+//menuId로 메뉴 조회
+export const getBookmarkedByMenuId=async(store_id, menu_id,pk_user)=>{
+    const query=`
+        SELECT menu.*,store.store_id, Bookmark.pk_user
+        FROM menu
+                 INNER JOIN store on store.store_id=menu.store_id
+                 INNER JOIN Bookmark on store.store_id=Bookmark.store_id
+        WHERE menu.store_id=? AND menu.id=? AND Bookmark.pk_user=?`;
+    const params=[store_id, menu_id, pk_user];
+    const [rows]=await pool.execute(query, params);
+
+    return rows;
+}
+
+export const getBookmarkedMeal=async(storeId, pk_user)=>{
+    const query=`
+        SELECT menu.id, menu.store_id, menu.menu_name, menu.price, menu.containter, menu.insulation, menu.liquid_seal, menu.about_menu, menu.image, Bookmark.pk_user
+        FROM menu 
+        INNER JOIN Bookmark on menu.store_id=Bookmark.store_id
+        WHERE menu.store_id=? AND menu.menu_category=1 AND Bookmark.pk_user=?`;
+    const param=[storeId];
+    const [rows]=await pool.execute(query, param);
+
+    return rows;
+}
+
+export const getBookmarkedSide=async(storeId)=>{
+    const query=`
+        SELECT menu.id, menu.store_id, menu.menu_name, menu.price, menu.containter, menu.insulation, menu.liquid_seal, menu.about_menu, menu.image, Bookmark.pk_user
+        FROM menu
+                 INNER JOIN Bookmark on menu.store_id=Bookmark.store_id
+        WHERE menu.store_id=? AND menu.menu_category=0 AND Bookmark.pk_user=?`;
+    const param=[storeId];
+    const [rows]=await pool.execute(query, param);
+
+    return rows;
+}
