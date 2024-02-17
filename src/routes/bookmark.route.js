fe075b3d7cd9d6086468fab5e41249744b8000c6
lumiere-on <mjw0214@ewhain.net>
2024-02-17 15:17:25 +0900

    Revise API url

--- src/routes/bookmark.route.js
+++ src/routes/bookmark.route.js
@@ -7,14 +7,14 @@ import {getBookmarkedStoreController, postBookmarkController,getBookmarkedByStor
 
 const bookmarkRouter=express.Router();
 
-bookmarkRouter.get('/bookmark/:pk_user',getBookmarkedStoreController);
-bookmarkRouter.post('/bookmark/:store_id/:pk_user',postBookmarkController);
+bookmarkRouter.get('/:pk_user',getBookmarkedStoreController);
+bookmarkRouter.post('/:store_id/:pk_user',postBookmarkController);
 
 
 //북마크에서 가게 접근
-bookmarkRouter.get('/order/bookmark/:pk_user/:store_id',getBookmarkedByStoreIdController);
-bookmarkRouter.get('/order/bookmark/:pk_user/:store_id/menu/:menu_id',getBookmarkedMenuIdController);
-bookmarkRouter.get('/order/bookmark/:pk_user/:store_id/meal',showBookmarkedMealController);
-bookmarkRouter.get('/order/bookmark/:pk_user/:store_id/side',showBookmarkedSideController);
+bookmarkRouter.get('/:pk_user/:store_id',getBookmarkedByStoreIdController);
+bookmarkRouter.get('/:pk_user/:store_id/menu/:menu_id',getBookmarkedMenuIdController);
+bookmarkRouter.get('/:pk_user/:store_id/meal',showBookmarkedMealController);
+bookmarkRouter.get('/:pk_user/:store_id/side',showBookmarkedSideController);
 
 export default bookmarkRouter;
--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -4,12 +4,12 @@ export const orderRouter=express.Router();
 import {getStoreListController,getByStoreIdController, getMenuController, showMealController, showSideController, searchByStoreNameController} from "../controllers/store.controller.js";
 
 //지도에서 가게 접근
-orderRouter.get('/order/map/near',getStoreListController);
-orderRouter.get('/order/map/near/:store_id',getByStoreIdController);
-orderRouter.get('/order/map/near/:store_id/menu/:menu_id}',getMenuController);
-orderRouter.get('/order/map/near/:store_id/meal',showMealController);
-orderRouter.get('/order/map/near/:store_id/side',showSideController);
-orderRouter.get('/order/map/search/:store_name', searchByStoreNameController);
+orderRouter.get('/near',getStoreListController);
+orderRouter.get('/near/:store_id',getByStoreIdController);
+orderRouter.get('/near/:store_id/menu/:menu_id}',getMenuController);
+orderRouter.get('/near/:store_id/meal',showMealController);
+orderRouter.get('/near/:store_id/side',showSideController);
+orderRouter.get('/search/:store_name', searchByStoreNameController);
 
 
 
