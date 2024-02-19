e2559526e56081aec65b166ceb010e670d5f36fe
0-x-14 <81631374+0-x-14@users.noreply.github.com>
2024-02-20 00:14:15 +0900

    [Fix] 카카오 로그인 API 수정 및 스웨거 추가 #140 (#142)
    * [Feature]가게명 검색 API (#138)
    
    * [Feat]Revise Search API
    
    * [Feature]Revise Search API
    
    * [Feature]Revise search API
    
    * [Fix] 카카오 로그인 API 수정 및 스웨거 추가 (#141)
    
    ---------
    
    Co-authored-by: lumiere-on <151502448+lumiere-on@users.noreply.github.com>

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,28 @@
-import { getKakaoId } from '../services/user.service.js';
+import { getUserById, signUp } from '../services/user.service.js';
 
-const signInKakao = async (req, res) => {
-    const headers = req.headers["authorization"];
-    const kakaoToken = headers.split(" ")[1];
-    // kakaoToken 변수에 액세스 토큰값 저장
-    // const kakaoToken = "임시 토큰 발급 받아서 수정하고 테스트";
+export const signInKakao = async (req, res) => {
+    const { kakaoId, email, name, profileImage } = req.body;
     
-    const accessToken = await getKakaoId(kakaoToken);
+    if (!name) throw new Error("이름 정보 누락", 400);
+    if (!email) throw new Error("이메일 정보 누락", 400);
+    if (!kakaoId) throw new Error("카카오 아이디 정보 누락", 400);
+    if (!profileImage) throw new Error("프로필 이미지 정보 누락", 400);
+    // 필수 정보가 누락됐을 경우 에러 발생
 
-    return res.status(200).json({ accessToken });
+    try {
+        const user = await getUserById(kakaoId);
+        // 데이터베이스에서 사용자 조회
+    
+        if (user.length === 0) {
+            await signUp(kakaoId, email, name, profileImage);
+        } // 데이터베이스에 사용자가 존재하지 않을 경우 사용자 생성 (회원가입)
+        else {
+            await getUserById(kakaoId);
+        } // 기존에 있던 회원일 경우
+
+        res.status(200).send({ message: '로그인 성공' });
+    } catch(error) {
+        console.error(error);
+        next (error);
+    }
 };
-
-export { signInKakao };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,11 @@
-import { getUserById, signUp } from "../services/user.dao.js";
-import axios from "axios";
-import jwt from "jsonwebtoken";
-const getKakaoId = async (kakaoToken) => {
+// import { getUserById, signUp } from "../services/user.dao.js";
+// import axios from "axios";
+// import jwt from "jsonwebtoken";
+import { pool } from '../../config/db.config.js';
+
+/* 기존 코드
+export const getKakaoId = async (kakaoId, email, name, profileImage) => {
+
     const result = await axios.get("https://kapi.kakao.com/v2/user/me", {
         headers: {
             Authorization: `Bearer ${kakaoToken}`,
@@ -15,7 +19,8 @@ const getKakaoId = async (kakaoToken) => {
     const email = data.kakao_account.email;
     const kakaoId = data.id;
     const profileImage = data.properties.profile_image;
-    if (!name || !email || !kakaoId) throw new Error("KEY_ERROR", 400);
+
+    if (!name || !email || !kakaoId || !profileImage) throw new Error("KEY_ERROR", 400);
     // 필수 정보가 누락됐을 경우 에러 발생
 
     const user = await getUserById(kakaoId);
@@ -26,8 +31,23 @@ const getKakaoId = async (kakaoToken) => {
     }
     // 데이터베이스에 사용자가 존재하지 않을 경우 사용자 생성
 
-    console.log("user 값은 : ", user);
-    return jwt.sign({ kakaoId: user[0].pk_user }, process.env.JWT_SECRET);
-};
+    next
+}; */
 
-export { getKakaoId };
+export const getUserById = async (kakaoId) => {
+    const query = 'SELECT pk_user, email, name, profile FROM User WHERE pk_user=?';
+    const params = [kakaoId]
+    const rows = await pool.execute(query, params);
+
+    return rows[0];
+}; // kakaoId 값으로 특정 사용자 조회
+
+export const signUp = async (kakaoId, email, name, profileImage) => {
+    const params = [kakaoId, name, email, name, profileImage];
+    // 카카오 로그인시 닉네임(카카오톡 닉네임)만 수집하기 때문에 name과 nickname 필드에 임의로 동일한 값을 넣는 방식으로 처리하였음
+    const query = 'INSERT INTO User(pk_user, name, email, nickname, profile, payment, social_login, phone_number) VALUES (?, ?, ?, ?, ?, "임시", 1, "임시")';
+    // const query = 'INSERT INTO User(kakaoId, name, email, name, profile, `임시`, 1, `임시`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    const rows = await pool.execute(query, params);
+
+    return rows[0];
+}; // 데이터베이스에 등록되지 않은 사용자인 경우 등록
